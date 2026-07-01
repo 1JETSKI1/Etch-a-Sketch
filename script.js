886156488df3738d3428349065106a1737b3c5ad
1JETSKI1 <261773881+1JETSKI1@users.noreply.github.com>
2026-07-01 22:43:10 +0200

    create grit loop

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
-const { createElement } = require("react")
-
 function createGrid() {
-    document.querySelector('.grid-container')
-    createElement
+    //grab container
+    const container = document.querySelector('.grid-container');
+    //loop 256 times
+    for (let i = 0; i < 256; i++) {
+        //create div each iteration
+        const gridItem = document.createElement('div');
+        container.appendChild(gridItem);
+    }
 }
+
+createGrid();
+
+
