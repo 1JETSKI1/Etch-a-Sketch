4536c58800d4978b39ae35d816787da0c8078a0e
1JETSKI1 <261773881+1JETSKI1@users.noreply.github.com>
2026-07-07 15:07:03 +0200

    add reset button

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-function createGrid() {
-    //grab container
+function createGrid(size) {
+    const totalSquares = size * size;
+    const squareSize = 960 / size;
     const container = document.querySelector('.grid-container');
-    //loop 256 times
-    for (let i = 0; i < 256; i++) {
-        //create div each iteration
+    container.innerHTML = '';
+    for (let i = 0; i < totalSquares; i++) {
         const gridItem = document.createElement('div');
-        container.appendChild(gridItem); 
+        container.appendChild(gridItem);
+        gridItem.style.width = squareSize + "px";
+        gridItem.style.height = squareSize + "px";
         gridItem.addEventListener('mouseover', function (e) {
             e.target.classList.add('hovered');
-        })
+        });
     }
-
 }
 
-createGrid();
+createGrid(16);
 
-
-
-
+const button = document.querySelector('#reset-button');
+button.addEventListener('click', function() {
+    const size = parseInt(prompt("How many squares per side?"), 10);
+    if (isNaN(size) || size > 100) {
+        alert("Please pick a number under 100");
+        return;
+    }
+    createGrid(size);
+    
+});
